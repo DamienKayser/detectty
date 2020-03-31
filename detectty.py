2962ac4b9e8489fd1e100fd2c8fbd0254d94cf65
Daniel Keller <daniel.keller@de.bosch.com>
2020-03-31 18:57:12 +0200

    Supporting usage as a python module.

--- detectty.py
+++ detectty.py
@@ -6,20 +6,24 @@ import time
 def getCurrentDevices():
     return ["/dev/" + dev for dev in os.listdir("/dev") if "tty" in dev]
 
-print("Waiting for tty devices to be plugged in ...", end="", flush=True)
-devBefore = getCurrentDevices()
-while(True):
-    time.sleep(0.5)
-    print(".", end="", flush=True)
-    devAfter = getCurrentDevices()
-    diff = [dev for dev in devAfter if dev not in devBefore]
-    if len(diff) > 0:
-        newDev = diff[0]
-        print("\n")
-        print("Detected new device:")
-        print(newDev)
-        print("")
-        print("Setting environment variable:")
-        os.environ["NEW_TTY_DEV"] = newDev
-        print("NEW_TTY_DEV='{0}'".format(os.environ["NEW_TTY_DEV"]))
-        exit(0)
+def detect():
+    print("Waiting for tty devices to be plugged in ...", end="", flush=True)
+    devBefore = getCurrentDevices()
+    while(True):
+        time.sleep(0.5)
+        print(".", end="", flush=True)
+        devAfter = getCurrentDevices()
+        diff = [dev for dev in devAfter if dev not in devBefore]
+        if len(diff) > 0:
+            newDev = diff[0]
+            print("\n")
+            print("Detected new device:")
+            print(newDev)
+            print("")
+            print("Setting environment variable:")
+            os.environ["NEW_TTY_DEV"] = newDev
+            print("NEW_TTY_DEV='{0}'".format(os.environ["NEW_TTY_DEV"]))
+            return newDev
+
+if __name__ == "__main__":
+    detect()
