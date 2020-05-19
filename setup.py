44cb9f488026aca7c3a8ac4c56a7c460a930fffc
Daniel Keller <daniel.keller@de.bosch.com>
2020-05-19 07:46:56 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="detectty",
+    version="1.0.0",
+    author="Daniel Keller",
+    author_email="develop@dkkeller.de",
+    description="Detects freshly plugged serial devices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkkeller/detectty.git",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts' : [
+            'detectty = detectty.detectty:detect',
+        ],
+    },
+    package_data={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)
