03b6277f49c582e4dfb0a1dc0bf885cfdcab85b6
Daniel Keller <daniel.keller@de.bosch.com>
2020-05-27 11:55:19 +0200

    Version 1.3.1
    Filtering devices correctly.

--- detectty/detectty.py
+++ detectty/detectty.py
@@ -4,6 +4,14 @@ import os
 import time
 from serial.tools import list_ports
 
+def _filterDevices(devices, filterDict):
+    def filterByDict(dev):
+        for key in filterDict:
+            if getattr(dev, key) != filterDict[key]:
+                return False
+        return True
+    return list(filter(filterByDict, devices))
+
 def getCurrentDevices(filterDict=None):
     """
     Returns a (filtered) list of currently connected serial devices.
@@ -12,20 +20,16 @@ def getCurrentDevices(filterDict=None):
     """
     devices = list_ports.comports()
     if filterDict is not None:
-        def filterByDict(dev):
-            for key in filterDict:
-                if getattr(dev, str(key)) != filterDict[key]:
-                    return False
-            return True
-        return list(filter(filterByDict, devices))
+        return _filterDevices(devices, filterDict)
     return devices
 
 def detect(filterDict=None, timeout=None):
     """
-    - If timeout is None: Wait for new device forever
-    - If timeout is set:
-        - Wait accordingly for new device
-        - If no new device found, filter current ones by filterDict, return first match
+    Detects new serial devices.
+    :param filterDict: The filter to apply
+    :type filterDict:  dict
+    :param timeout: The timeout value (in seconds)
+    :type timeout:  float
     """
     print("Waiting for device to be plugged in ...", end="", flush=True)
     devBefore = getCurrentDevices()
@@ -35,13 +39,22 @@ def detect(filterDict=None, timeout=None):
         devAfter = getCurrentDevices()
 
         # If new device is detected - go for it!
-        diff = [dev.device for dev in devAfter if dev not in devBefore]
+        diff = [dev for dev in devAfter if dev not in devBefore]
         if len(diff) > 0:
             newDev = diff[0]
             print("\n")
             print("Detected new device:")
             print(newDev)
-            return newDev
+            if filterDict is None:
+                return newDev.device
+            else:
+                filteredDevices = _filterDevices([newDev], filterDict)
+                if len(filteredDevices) > 0:
+                    return filteredDevices[0].device
+                else:
+                    devBefore = devAfter
+                    # Not the device we are looking for. Continue ...
+                    continue
 
         # If timeout is set: check devices by filter, if no new device was detected
         # If timeout is not set: wait for new device infinitely
@@ -50,10 +63,15 @@ def detect(filterDict=None, timeout=None):
                 timeout -= .5
                 continue
 
+            # Timed out
             if filterDict is None:
                 # Pretty pointless ;-)
-                return None
-            return getCurrentDevices(filterDict=filterDict)[0]
+                return []
+            filteredDevices = getCurrentDevices(filterDict=filterDict)
+            if len(filteredDevices) > 0:
+                return filteredDevices[0].device
+            return []
+
 
 if __name__ == "__main__":
     detect()
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="detectty",
-    version="1.3.0",
+    version="1.3.1",
     author="Daniel Keller",
     author_email="develop@dkkeller.de",
     description="Detects freshly plugged serial devices.",
